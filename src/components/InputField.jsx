e9d377e59fab2acf9b73500508c1a39741c944ab
Tu Hoang <tuhoangz2k@gmail.com>
2022-10-09 22:13:46 +0700

    invalid form

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-function InputField({ icon, type = 'type', placeholder }) {
+
+function InputField({ icon, type = 'type', placeholder, register, name, errors }) {
+    console.log(errors);
     return (
-        <div className="flex items-center gap-2 mt-4 border-b-2 border-[#333]">
-            {icon}
-            <input
-                type={type}
-                placeholder={placeholder}
-                className="bg-transparent text-white text-[18px] outline-none py-2 px-1"
-            />
+        <div className="">
+            <div className="flex items-center gap-2 mt-4 border-b-2 border-[#333]">
+                {icon}
+                <input
+                    type={type}
+                    placeholder={placeholder}
+                    className="bg-transparent text-white text-[18px] outline-none py-2 px-1"
+                    {...register(name)}
+                />
+            </div>
+            {errors && <p className="text-[#df2b4f] mt-2 font-[500]">{errors.message}</p>}
         </div>
     );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,37 +2,86 @@ import { useState } from 'react';
 import InputField from '../components/InputField';
 import { UserAuth } from '../context/Auth';
 import { AiTwotoneMail, AiFillLock } from 'react-icons/ai';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import ramRem from '../assets/img/ramRem.png';
+
+const schema = yup
+    .object({
+        email: yup
+            .string()
+            .required('email is required')
+            .email('email is invalid')
+            .min(6, 'Email must have at least 6 characters'),
+        password: yup
+            .string()
+            .required('password is required')
+            .min(6, 'Password must be at least 6 characters'),
+    })
+    .required();
 
 function SignUp(props) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        control,
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+
     const { createUser } = UserAuth();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        createUser(email, password);
+    const handleSubmitForm = async (data) => {
+        console.log(data);
+        //    await createUser(email, password);
     };
 
     return (
         <div className="flex flex-col items-center h-[100%] mt-[30vh] translate-y-[-50%]">
-            <h4 className="text-[34px] text-white font-semibold">Sign Up</h4>
-            <form onSubmit={handleSubmit}>
-                <InputField
-                    placeholder="Enter your email"
-                    type="email"
-                    icon={<AiTwotoneMail size={18} />}
-                />
-                <InputField
-                    placeholder="Password"
-                    type="password"
-                    icon={<AiFillLock size={18} />}
-                />
-                <button
-                    type="submit"
-                    className="text-[#333] bg-[#d8d8d8] mt-8 w-full md:w-[150px] md:mx-[50%] md:translate-x-[-50%] h-[45px] rounded-sm font-semibold"
+            <div className="absolute h-[500px] z-[1]">
+                <img src={ramRem} alt="" className="w-full h-full object-cover" />
+            </div>
+            <div className="relative z-[5] w-full">
+                <h4 className="text-[34px] text-white font-semibold text-center">Sign Up</h4>
+                <form
+                    onSubmit={handleSubmit(handleSubmitForm)}
+                    className="w-[80%] md:w-[60%] lg:w-[400px] mx-auto"
                 >
-                    create
-                </button>
-            </form>
+                    <InputField
+                        placeholder="Enter your email"
+                        type="email"
+                        icon={<AiTwotoneMail size={18} />}
+                        register={register}
+                        state={email}
+                        setState={setEmail}
+                        errors={errors.email}
+                        name="email"
+                        control={control}
+                    />
+                    <InputField
+                        placeholder="Password"
+                        type="password"
+                        icon={<AiFillLock size={18} />}
+                        register={register}
+                        name="password"
+                        state={password}
+                        setState={setPassword}
+                        errors={errors.password}
+                        control={control}
+                    />
+                    <button
+                        type="submit"
+                        className="text-[#333] bg-[#d8d8d8] mt-8 w-full md:w-[150px] 
+                        md:mx-[50%] md:translate-x-[-50%] h-[45px] 
+                        rounded-sm font-semibold hover:opacity-80 transition-all hover:text-[red]"
+                    >
+                        Sign Up
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
